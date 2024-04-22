4127666de63a7009542a68f42389af350ed23c31
x00167885 <x00167885@mytudublin.ie>
2024-04-22 13:30:40 +0100

    Small button

--- app/src/main/java/com/example/eventplanner/PersonDetails.java
+++ app/src/main/java/com/example/eventplanner/PersonDetails.java
@@ -2,6 +2,7 @@ package com.example.eventplanner;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
